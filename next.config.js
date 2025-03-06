9fd290fd5852379b0afe3db9a6db487537713392
Yuxas <gungamwing@gmail.com>
2025-03-06 13:39:48 +0900

    Update configuration for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === 'production' ? '/SHKHP' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/SHKHP/' : '',
 };
 
 module.exports = nextConfig;
